2cd3a76c662313ffe45b8fcc86f8693983b992ac
musa <msigwamb@gmail.com>
2023-03-26 12:05:58 +0300

    topanel1

--- src/app/page.js
+++ src/app/page.js
@@ -260,7 +260,7 @@ const time = useCountdown(deadline);
             <a href="#faq" className="cursor-pointer hope text-lg  px-2 mr-3 rounded-md">
     <span className=" relative group ">
 <span className="">FAQ's</span>
-{/* git commit -m "27/02/2023 5" */}
+{/* git commit -m "27/02/2023 6" */}
   <span className="absolute -bottom-1 left-0 w-0 h-[0.2em] bg-[#339966] transition-all group-hover:w-[2.7em]"></span>
 </span>
 
@@ -839,7 +839,7 @@ So what are you waiting for? Sign up for our online courses today and start your
 
 <div id ="faq" className="max-w-7xl mx-auto px-4 my-20 sm:px-6 ">
 <div className="text-center">
-<h1 className="text-7xl font-serif  text-white " >FAQ'S</h1>
+<h1 className="text-4xl font-serif  text-white " >FAQ'S</h1>
 </div>
 <Accordion className="mt-4 border border-gray-800 border-3xl">
   <Accordion.Panel  className="border border-sm">
